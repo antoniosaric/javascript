cc927f58d0d912b30536bb9ff9ddc0ec9009afaa
Tony <saric.tony@gmail.com>
2017-05-31 10:14:54 -0700

    adds while

---
+++ javascript2.js
@@ -0,0 +1,46 @@
+var luckyNumber = 8;
+
+switch(luckyNumber){
+  case 1:
+    console.log("is 1");
+    break;
+  case 8: 
+    console.log("is 8");
+    break;
+  default: 
+    console.log("default");
+    break;
+}
+
+// for (var i = 0; i < 5; i++){
+//   for (var j =0; j <2; j++){
+//     console.log(i + " : " + j);
+//   }
+// }
+
+for (var i = 0; i<5; i++){
+  if (i == 1){
+    console.log("1");
+  }
+}
+
+// for (var i = 0; i < 5; i++){
+//   for(var j = 0; j < 2; j++){
+
+//     if (i == 1){
+//       continue;
+//     }
+//   }
+//   console.log(i);
+// }
+
+var number = 5;
+while (number < 7){
+  console.log("last");
+  console.log(number);
+  number++
+}
+
+
+
+
