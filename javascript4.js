220c8bb3ff5717a81f9119d2dfc9f1711181c410
Tony <saric.tony@gmail.com>
2017-05-31 12:28:02 -0700

    adds prototype

---
+++ javascript4.js
@@ -0,0 +1,29 @@
+function person (args) {
+  this.name = args.name,
+  this.age = args.age,
+  this.details = {
+    hobbies: ['sports', 'cookies'],
+    location: 'germany'
+  },
+this.greet = function() {
+  return "purple";
+}
+};
+
+// console.log(person);
+// console.log(person.name);
+// console.log(person.details.hobbies);
+// var answer = person.greet();
+// console.log(answer);
+
+// person.name = "bill";
+// console.log(person.name);
+
+var anotherPerson = new person("anna", 30);
+console.log(anotherPerson);
+
+person.prototype.stan = function(){
+  console.log("blahhhh");
+}
+
+anotherPerson.stan();
