98306c0e0059488c7e0260a3cf4c5f91a063c6ca
Tony <saric.tony@gmail.com>
2017-06-03 22:04:31 -0700

    reindents

--- javas.js
+++ javas.js
@@ -15,18 +15,18 @@
     var httpRequest = new XMLHttpRequest();
 
     httpRequest.onreadystatechange = function(){
-  if (this.readyState === XMLHttpRequest.DONE && this.status === 200){
-    console.log(JSON.parse(this.responseText)[0].title)
-    document.getElementById('header').append(JSON.parse(this.responseText)[0].title)
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200){
+        console.log(JSON.parse(this.responseText)[0].title)
+        document.getElementById('header').append(JSON.parse(this.responseText)[0].title)
+        document.getElementById('header').innerHTML = response.name + "<br>" + response.data + "<br>" + response.response;
+      }else if (this.readyState === XMLHttpRequest.DONE && this.status !== 200){
+        console.log("ERROR");
+      }
 
-  }else if (this.readyState === XMLHttpRequest.DONE && this.status !== 200){
-    console.log("ERROR");
-  }
-
-  };
+    };
     httpRequest.open('POST', 'ph.php', true)
     httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     httpRequest.send(myPost);
-}
+  }
 })();
 
