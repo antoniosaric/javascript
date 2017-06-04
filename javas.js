95ab75ed3e3298cdc1a052d07b3648e09edd7dc2
Tony <saric.tony@gmail.com>
2017-06-03 21:24:14 -0700

    adds get request with ajax

--- javas.js
+++ javas.js
@@ -1,2 +1,23 @@
-var httpRequest = new XMLHttpRequest();
-console.log(httpRequest)
+
+
+
+(function(){
+  document.getElementById('button').onclick = showData;
+  function showData(){
+  var httpRequest = new XMLHttpRequest();
+
+  httpRequest.onreadystatechange = function(){
+  if (this.readyState === XMLHttpRequest.DONE && this.status === 200){
+    console.log(JSON.parse(this.responseText)[0].title)
+    document.getElementById('header').append(JSON.parse(this.responseText)[0].title)
+
+  }else if (this.readyState === XMLHttpRequest.DONE && this.status !== 200){
+    console.log("ERROR");
+  }
+
+  };
+    httpRequest.open('GET', 'http://jsonplaceholder.typicode.com/posts', true)
+  httpRequest.send();
+}
+})();
+
